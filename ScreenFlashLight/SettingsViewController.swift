6b917193dfb357a66d40f41896e7addbc658254f
hieutam <nhoxbypass@gmail.com>
2017-04-25 23:19:39 +0700

    implement transfer data back to main view controller

--- ScreenFlashLight/SettingsViewController.swift
+++ ScreenFlashLight/SettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITextFieldDelegate {
 
     
     @IBOutlet weak var redSlider: UISlider!
@@ -21,29 +21,69 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var blueTxtField: UITextField!
     @IBOutlet weak var alphaTxtField: UITextField!
     
+    @IBOutlet weak var previewView: UIView!
+    
+    @IBAction func onSaveButtonTapped(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "ToMainAndSaveSegue", sender: self)
+    }
+    
+    @IBAction func onBackButtonTapped(_ sender: Any) {
+        _ = self.navigationController?.popViewController(animated: true)
+    }
+    
     @IBAction func onRedSliderValueChanged(_ sender: UISlider) {
+        red = redSlider.value
+        redTxtField.text = "\(red *  255)"
+        updatePreviewColor()
     }
     
     @IBAction func onGreenSliderValueChanged(_ sender: UISlider) {
+        green = greenSlider.value
+        greenTxtField.text = "\(green *  255)"
+        updatePreviewColor()
     }
     
     @IBAction func onBlueSliderValueChanged(_ sender: UISlider) {
+        blue = blueSlider.value
+        blueTxtField.text = "\(blue *  255)"
+        updatePreviewColor()
     }
     
     @IBAction func onAlphaSliderValueChanged(_ sender: UISlider) {
+        alpha = alphaSlider.value
+        alphaTxtField.text = "\(alpha *  255)"
+        updatePreviewColor()
     }
     
     //MARK: *** Data model
     var red: Float = 0.5
     var green: Float = 0.5
     var blue: Float = 0.5
-    var alpha: Float = 0.5
-    var defaultColor: UIColor?
+    var alpha: Float = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        previewView.backgroundColor = UIColor(red:0.5,green:0.5,blue: 0.5,alpha:1)
+        
+        if (red != nil){
+            redSlider.value = Float(CGFloat((red)))
+            greenSlider.value = Float(CGFloat((green)))
+            blueSlider.value = Float(CGFloat((blue)))
+            alphaSlider.value = Float(CGFloat((alpha)))
+            
+            updatePreviewColor()
+            redTxtField.text = "\( redSlider.value *  255)"
+            greenTxtField.text = "\( greenSlider.value *  255)"
+            blueTxtField.text = "\( blueSlider.value *  255)"
+            alphaTxtField.text = "\( alphaSlider.value *  255)"
+            
+            redTxtField.delegate = self
+            greenTxtField.delegate = self
+            blueTxtField.delegate = self
+            alphaTxtField.delegate = self
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,15 +91,64 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    //Logic functions
+    func updatePreviewColor(){
+        previewView.backgroundColor = UIColor(red:CGFloat(red),green:CGFloat(green),blue: CGFloat(blue),alpha:CGFloat(alpha))
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var redValue = redTxtField.text!
+        var greenValue = greenTxtField.text!
+        var blueValue = blueTxtField.text!
+        var aphaValue = alphaTxtField.text!
+        
+        if (textField == redTxtField){
+            redValue += string
+            redSlider.value = (redValue as! NSString).floatValue / 255
+            red = redSlider.value
+            updatePreviewColor()
+        }
+        else if (textField == greenTxtField){
+            greenValue += string
+            greenSlider.value = (greenValue as! NSString).floatValue / 255
+            green = greenSlider.value
+            updatePreviewColor()
+        }
+        else if (textField == blueTxtField){
+            blueValue += string
+            blueSlider.value = (blueValue as! NSString).floatValue / 255
+            blue = blueSlider.value
+            updatePreviewColor()
+        }
+        else if (textField == alphaTxtField){
+            aphaValue += string
+            alphaSlider.value = (aphaValue as! NSString).floatValue / 255
+            alpha = alphaSlider.value
+            updatePreviewColor()
+        }
+        
+        return true
+    }
+    
 
-    /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if (segue.identifier == "ToMainAndSaveSegue"){
+            let destination = segue.destination as! ViewController
+            
+            destination.currColor = UIColor(red:CGFloat(red),green:CGFloat(green),blue: CGFloat(blue),alpha:CGFloat(alpha))
+            UserDefaults.standard.set(red, forKey: "red")
+            UserDefaults.standard.set(green, forKey: "green")
+            UserDefaults.standard.set(blue, forKey: "blue")
+            UserDefaults.standard.set(alpha, forKey: "alpha")
+            
+            
+        }
     }
-    */
 
 }
--- ScreenFlashLight/ViewController.swift
+++ ScreenFlashLight/ViewController.swift
@@ -10,18 +10,58 @@ import UIKit
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var mainView: UIView!
+    
+    
     @IBOutlet weak var mUIView: UIView!
+    var currColor:UIColor?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.uiViewTapped))
+        let red = UserDefaults.standard.value(forKeyPath: "red")
+        let green = UserDefaults.standard.value(forKeyPath: "green")
+        let blue = UserDefaults.standard.value(forKeyPath: "blue")
+        let alpha = UserDefaults.standard.value(forKeyPath: "alpha")
+        
+        if (red != nil) {
+            currColor = UIColor(red: red as! CGFloat,green:green as! CGFloat,blue: blue as! CGFloat,alpha:alpha as! CGFloat)
+        } else {
+            currColor = UIColor(red:0.5, green:0.5, blue: 0.5, alpha: 0.5)
+            
+        }
+        
+        if (currColor != nil){
+            mainView.backgroundColor = currColor
+        }
+        
+        let hold = UILongPressGestureRecognizer(target: self, action: #selector(self.view_LongPressed))
+        self.view.addGestureRecognizer(hold)
+        
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.uiViewTapped))
         tap.delegate = self
         
-        mUIView.addGestureRecognizer(tap)
+        mainView.addGestureRecognizer(tap)
     }
     
+    func view_LongPressed(sender: UITapGestureRecognizer){
+        var timer = Timer()
+        
+        if sender.state == .began{
+            timer = Timer.scheduledTimer(timeInterval:0.05,target: self,selector: #selector(eachInterval),userInfo:nil,repeats:true)
+        }else if sender.state == .ended{
+            timer.invalidate()// dung timer
+            self.mainView.backgroundColor = currColor
+        }
+        
+    }
+    var black = true
     
+    func eachInterval(){
+        self.mainView.backgroundColor = black ? UIColor.black : currColor
+        black = !black
+    }
+
     // Receive action
     func uiViewTapped(gr:UITapGestureRecognizer)
     {
@@ -32,6 +72,9 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
 
     // MARK: - Navigation
     
@@ -39,15 +82,20 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if (segue.identifier == "ToSettingsSegue"){
-            let setting = segue.destination as! SettingsViewController
+            let settingVC = segue.destination as! SettingsViewController
             
             var red: CGFloat = 0
             var green: CGFloat = 0
             var blue: CGFloat = 0
             var alpha:  CGFloat = 0
             
-            
+            currColor?.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+            settingVC.red = Float(red)
+            settingVC.green = Float(green)
+            settingVC.blue = Float(blue)
+            settingVC.alpha = Float(alpha)
         }
+        
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
